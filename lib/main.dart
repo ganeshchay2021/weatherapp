2aa61765b10e0c351a643bcf3fbd30ac86822185
ganeshchay2021 <ganeshchay2021@gmail.com>
2024-06-21 05:51:36 +0545

    upgraded splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:weatherapp/pages/home%20screen/ui/homes_screen.dart';
 import 'package:weatherapp/repository/weather_repository.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  FlutterNativeSplash.remove();
   runApp(
     const MyApp(), // Wrap your app
   );
